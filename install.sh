182ea8984fc7c5f3ed85a0f4419c63d7bcba44e2
Bogdan <bgd.draghia@gmail.com>
2022-10-19 19:32:03 +0000

    update install script, debian distro

--- install.sh
+++ install.sh
@@ -19,7 +19,7 @@ echo "-----------------------"
 echo "Installing tmux"
 
 apt-get install tmux -y
-
+  cp -r .config/tmux/ ~/.config/tmux/
 echo "-----------------------"
 echo "Installing fish"
 
@@ -27,23 +27,25 @@ echo 'deb http://download.opensuse.org/repositories/shells:/fish:/release:/3/Deb
 wget -q -O - https://download.opensuse.org/repositories/shells:fish:release:3/Debian_11/Release.key | sudo apt-key add -
 sudo apt update
 sudo apt install fish -y
-
-
 echo "-----------------------"
 echo "Making fish default shell"
 chsh --shell $(which fish)
 sudo chsh --shell $(which fish)
+echo "Install fish plugin manager"
+fish -c 'curl -sL https://git.io/fisher | source && fisher install jorgebucaran/fisher'
+echo "Installing tide with default config"
+fish -c 'fisher install IlanCosman/tide@v5'
 
 echo "-----------------------"
 echo "Installing nvim manual"
-
-
 if [ -f "$FILE" ]; then
   # Take action if $DIR exists. #
   echo "nvim exist, skipping..."
   # Cleaning
   rm ./nvim.appimage 2>/dev/null
   rm -r squashfs-root 2>/dev/null
+  # Updating nvim plugins 
+  nvim --headless -c 'autocmd User PackerComplete quitall' -c 'PackerSync'
 else
   echo "-----------------------"
   echo "Installing nvim manual"
@@ -57,4 +59,14 @@ else
   # CLEANING 
   rm ./nvim.appimage
   rm -r squashfs-root
+  # Install packer
+  git clone --depth 1 https://github.com/wbthomason/packer.nvim\
+ ~/.local/share/nvim/site/pack/packer/start/packer.nvim
+  # Move dotfile to ~/.config user
+  cp -r .config/nvim/ ~/.config/nvim/
+  # Install plugins
+  nvim --headless -c 'autocmd User PackerComplete quitall' -c 'PackerSync'
+  nvim --headless -c 'autocmd User PackerComplete quitall' -c 'PackerSync'
 fi
+
+echo 'Script executed succesfully'
